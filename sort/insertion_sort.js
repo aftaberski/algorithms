f98e013aa10ed21af39c3d08b119107602683128
Anna Taberski <aftaberski@gmail.com>
2015-04-02 09:39:37 -0400

    Complete insertion sort

--- sort/insertion_sort.js
+++ sort/insertion_sort.js
@@ -6,3 +6,15 @@ var insert = function(array, rightIndex, value) {
     }
     array[index + 1] = value;
 };
+
+var insertionSort = function(array) {
+    for(var i = 1; i < array.length; i++){
+        var value = array[i];
+        var rightIndex = i - 1;
+        insert(array, rightIndex, value);
+    }
+};
+
+var array = [22, 11, 99, 88, 9, 7, 42];
+insertionSort(array);
+console.log("Array after sorting:  " + array);
