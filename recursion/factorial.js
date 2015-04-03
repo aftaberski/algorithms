e250e7262d977f758c895d80a74bb11a0a66f25d
Anna Taberski <aftaberski@gmail.com>
2015-04-02 21:24:51 -0400

    Add recursive factorial solution

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -1,3 +1,11 @@
+// RECURSIVE
+var factorial = function(n) {
+  if (n === 0){
+      return 1;
+  }
+  return n * factorial(n-1);
+};
+
 // ITERATIVE
 var factorial = function(n) {
     var result = 1;
