8b94d69a85fefeca6dca562e08aab90b7b8abd4d
aftaberski <aftaberski@gmail.com>
2015-05-27 18:36:17 -0400

    WIP JS solution

---
+++ other/merge_ranges.js
@@ -0,0 +1,9 @@
+function mergeRanges(ranges) {
+    var sortedRanges = ranges.sort(x, y);
+    var mergedRanged = [];
+
+    return sortedRanges;
+}
+
+ranges = [(1, 5), (7, 8), (1, 2)];
+console.log(mergeRanges(ranges));
