4a39fdd379a22afb299e149c7ec36ebd5c4af670
Anna Taberski <aftaberski@gmail.com>
2015-05-28 09:05:22 -0400

    Add function to sort ranges

--- other/merge_ranges.js
+++ other/merge_ranges.js
@@ -1,9 +1,16 @@
 function mergeRanges(ranges) {
-    var sortedRanges = ranges.sort(x, y);
+    var sortedRanges = ranges.sort(function(a, b){
+        if(a[0] === b[0]){
+          return a[1] - b[1];
+        }
+        else  {
+          return a[0] - b[0];
+        }
+      });
     var mergedRanged = [];
 
     return sortedRanges;
 }
 
-ranges = [(1, 5), (7, 8), (1, 2)];
+ranges = [[1, 5], [7, 8], [1, 2]];
 console.log(mergeRanges(ranges));
